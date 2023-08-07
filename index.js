18bf3ff078029b09a1a687c341adeead0911e702
andre <132362796+andrelima-ifms@users.noreply.github.com>
2023-08-07 16:27:26 -0400

    criação da função subtração

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const soma = (a, b) => {
   return a + b;
 }
 
-let resultado = soma(7, 11);
-console.log(resultado);
+const subtração = (a, b) => {
+  return a - b;
+}
 
+let res_soma = soma(7, 11);
+console.log(res_soma);
+
+
+let res_sub = soma(10, 15);
+console.log(res_sub);
